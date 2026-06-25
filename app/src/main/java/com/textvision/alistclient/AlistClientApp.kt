1ef30c792d471222038953784c200419a5665324
贾晓源 <fectivnfy@163.com>
2026-06-25 22:57:28 +0800

    refactor: dedupe markInterruptedOnStartup to MainActivity

--- app/src/main/java/com/textvision/alistclient/AlistClientApp.kt
+++ app/src/main/java/com/textvision/alistclient/AlistClientApp.kt
@@ -1,22 +1,11 @@
 package com.textvision.alistclient
 
 import android.app.Application
-import com.textvision.alistclient.transfer.TransferManager
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @HiltAndroidApp
 class AlistClientApp : Application() {
-    @Inject lateinit var transferManager: TransferManager
-
-    private val startupScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
-
     override fun onCreate() {
         super.onCreate()
-        startupScope.launch { transferManager.markInterruptedOnStartup() }
     }
 }
