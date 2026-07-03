ddccd9cce969932952e31f4ac856b117aaf9f898
贾晓源 <fectivnfy@163.com>
2026-07-03 17:11:23 +0800

    feat(home): add SectionResult sealed type and SectionKey enum

---
+++ app/src/main/java/com/textvision/alistclient/home/SectionKey.kt
@@ -0,0 +1,3 @@
+package com.textvision.alistclient.home
+
+enum class SectionKey { Public, Storage, ServerStats, Session, Task }
---
+++ app/src/main/java/com/textvision/alistclient/home/dto/SectionResult.kt
@@ -0,0 +1,13 @@
+package com.textvision.alistclient.home.dto
+
+sealed interface SectionResult<out T> {
+    data class Ok<T>(val data: T) : SectionResult<T>
+    data object Loading : SectionResult<Nothing>
+    data class Failed(val cause: SectionFailure) : SectionResult<Nothing>
+}
+
+sealed interface SectionFailure {
+    data object Network : SectionFailure
+    data object Unauthorized : SectionFailure
+    data class Server(val code: Int) : SectionFailure
+}
